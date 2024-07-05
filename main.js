fb7ac7837d13b6551e84347a68a5b7c87236ac01
Kenenias Perez <kenenias.bethuel@gmail.com>
2024-07-05 13:22:15 -0600

    se agregan eventos al programa de aluramidi

--- main.js
+++ main.js
@@ -3,17 +3,25 @@ function playSonido(idElementoAudio){
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+console.log(listaDeTeclas);
 
 for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    
+
     const idAudio = `#sonido_${instrumento}`  
     console.log(idAudio);
 
     tecla.onclick = function(){
         playSonido(idAudio);
     };
+
+    tecla.onkeydown = () => {
+        tecla.classList.add('activa');
+    }
+    tecla.onkeyup = () => {
+        tecla.classList.remove('activa');
+    }
 }
 
 
